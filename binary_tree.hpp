23d619dafbf7a81444a430d53e695e3ea67f2876
7agm <109569781+7agm@users.noreply.github.com>
2020-02-11 10:14:46 +0000

    added pics

--- binary_tree.hpp
+++ binary_tree.hpp
@@ -5,3 +5,33 @@
 #include <cstdlib>
 #include <memory>
 #include <cassert>
+
+namespace ds_exp
+{
+    inline namespace tree
+    {
+        struct right_first_t;
+        struct left_first_t
+        {
+            constexpr left_first_t() = default;
+            using inverse = right_first_t;
+        };
+        struct right_first_t
+        {
+            constexpr right_first_t() = default;
+            using inverse = left_first_t;
+        };
+        using left_t = left_first_t;
+        using right_t = right_first_t;
+        constexpr left_first_t left_first;
+        constexpr left_t left_child;
+        constexpr right_first_t right_first;
+        constexpr right_t right_child;
+
+        template <typename T>
+        struct node
+        {
+            using value_type = T;
+
+            template <typename U>
+            explicit node(U &&value, node *parent, std::unique_ptr<node> left,
