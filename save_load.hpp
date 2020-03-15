0183119e89d30be07ecb700bcdc671fb0be7227d
7agm <109569781+7agm@users.noreply.github.com>
2020-03-15 21:19:09 +0000

    sched: Remove stale power aware scheduling remnants and dysfunctional knobs

--- save_load.hpp
+++ save_load.hpp
@@ -10,3 +10,15 @@ namespace ds_exp
     {
         template <typename T>
         std::istream &operator>>(std::istream &in, ds_exp::binary_tree<T> &tree)
+        {
+            tree = ds_exp::tree_parse<ds_exp::left_first_t, T>(in).get_binary_tree().value();
+            return in;
+        }
+
+        template <typename T, typename ...Escaped>
+        std::ostream &escape(std::ostream &out, T const &t, Escaped ...escaped)
+        {
+            std::ostringstream stream;
+            stream << t;
+            auto output = stream.str();
+            for (auto iter = output.begin(); iter != output.end(); ++iter)
