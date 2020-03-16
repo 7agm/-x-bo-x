e1fe5158b225a84a4323e75b581c655c8ca5f99c
7agm <109569781+7agm@users.noreply.github.com>
2020-03-16 21:30:12 +0000

    refactor of IFeatureSource and its fallout

--- save_load.hpp
+++ save_load.hpp
@@ -44,3 +44,21 @@ namespace ds_exp
                 print_null(out);
                 return;
             }
+            out << "(";
+            escape(out, *iter, ')') << ")";
+            print_node(out << ",", iter.first_child());
+            print_node(out << ",", iter.second_child());
+        }
+        template <typename T>
+        std::ostream &operator<<(std::ostream &out, binary_tree<T> const &tree)
+        {
+            out << "[";;
+            auto iter = tree.begin(preorder);
+            print_node(out, iter);
+            out << "]";
+            return out;
+        }
+    }
+}
+
+#endif //INC_201703_SAVE_LOAD_HPP
---
+++ test/test_binary_tree.cpp
@@ -0,0 +1,7 @@
+
+#include <string>
+#include "test_binary_tree.hpp"
+#include "../binary_tree.hpp"
+
+void test_binary_tree()
+{
