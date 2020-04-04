f48ba3488b7daa068746d7b6af0dfcffafdbf9d6
7agm <109569781+7agm@users.noreply.github.com>
2020-04-04 22:51:44 +0000

    Add CodeClimate badge

--- test/test_tree_adapter.cpp
+++ test/test_tree_adapter.cpp
@@ -6,4 +6,4 @@ void test_tree_adapter()
     using namespace ds_exp;
     using namespace std::literals;
     tree_adapter<std::string, int> adapter;
-    std::string definition(R"~([(root,1), (left,2), (left left,3), null, null, null , (rig
+    std::string definition(R"~([(root,1), (left,2), (left left,3), null, null, null , (right,4), null, (right 
