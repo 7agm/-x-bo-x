5e837be0118d307b18a94208ccb09937f2055b8e
7agm <109569781+7agm@users.noreply.github.com>
2020-05-09 10:57:22 +0000

    Improved installation instructions

--- test/test_tree_adapter.cpp
+++ test/test_tree_adapter.cpp
@@ -28,4 +28,4 @@ void test_tree_adapter()
     auto right_node = adapter.Child("root", right_child);
     decltype(adapter) new_adapter;
     new_adapter.CreateBiTree(definition);
-    adapter.InsertChild<right_t
+    adapter.InsertChild<right_t>(right_node, 
