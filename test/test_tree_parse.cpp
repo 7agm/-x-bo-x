f7d285c0dbc57a26a42c388be01312f1a69e20a6
7agm <109569781+7agm@users.noreply.github.com>
2020-05-19 19:45:14 +0000

    sm-110 Реализация мод workflow-zf2-toolkit

--- test/test_tree_parse.cpp
+++ test/test_tree_parse.cpp
@@ -4,3 +4,23 @@
 #include "test_tree_parse.hpp"
 #include "../tree_parse.hpp"
 #include "../tree_adapter.hpp"
+
+void test_tree_parse()
+{
+    using namespace ds_exp;
+    using namespace std::literals;
+    std::istringstream istream(R"~( [ (\,, 1), ([, 2), (\\,3), null, null, null, (\),4), null, null] )~"s);
+    tree_parse<left_first_t, adapter::detail::stored_t<std::string, int>> parse(istream);
+    auto tree = parse.get_binary_tree().value();
+    auto root = tree.root<preorder_t>();
+    auto iter = root;
+    assert(root->key == ",");
+    assert(root->value == 1);
+    ++iter;
+    assert(iter->key == "[");
+    assert(iter->value == 2);
+    ++iter;
+    assert(iter->key == "\\");
+    assert(iter->value == 3);
+    ++iter;
+    assert(iter->key == ")");
