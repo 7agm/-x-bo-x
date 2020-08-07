b8b574133610c7a9fd525e0fe7bc4a2a9c93e15c
7agm <109569781+7agm@users.noreply.github.com>
2020-08-07 14:17:17 +0000

    Rename license.txt to LICENSE.txt

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -57,4 +57,4 @@ namespace ds_exp
             template <typename t1, typename t2>
             struct support_equality
             {
-                template <typename r1, typename r2, typename Ret = decltype(get_k
+                template <typename r1, typename r2, typename Ret = decltype(get_key(std::declval<r1>()) ==
