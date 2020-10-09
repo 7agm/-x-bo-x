82ee19ed5c6816161100579b125652e223fad471
7agm <109569781+7agm@users.noreply.github.com>
2020-10-09 19:48:06 +0000

    Fix copypasta

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -74,4 +74,5 @@ namespace ds_exp
             bool operator==(t1 const &lhs, t2 const &rhs)
             {
                 return !(get_key(lhs) < get_key(rhs)) && !(get_key(rhs) < get_key(rhs));
-     
+            }
+           
