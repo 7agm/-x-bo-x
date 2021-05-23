51b0fbd16df1fbba979f2568d96ac7412ae82c16
7agm <109569781+7agm@users.noreply.github.com>
2021-05-23 06:11:07 +0000

    Updated '_layouts/post.html' via CloudCannon

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -134,4 +134,6 @@ namespace ds_exp
         using namespace std::literals;
 
         template <typename Key_t, typename Value_t = null_value_tag>
-        class tree_ada
+        class tree_adapter
+        {
+  
