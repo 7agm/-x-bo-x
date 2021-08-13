e98c62bbfe2400c5c01d1c3385cc55ec74582f8b
7agm <109569781+7agm@users.noreply.github.com>
2021-08-13 05:45:32 +0000

    New folder structure

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -159,4 +159,4 @@ namespace ds_exp
             struct tree_not_exist : std::logic_error
             {
                 explicit tree_not_exist(std::string const &function)
-                    : logic_error("The tree doesn't exist in function
+                    : logic_error("The tree doesn't exist in function "s + function + "."
