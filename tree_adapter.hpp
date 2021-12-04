08ae5547c34413def737b829c1534c9280d8bac9
7agm <109569781+7agm@users.noreply.github.com>
2021-12-04 20:19:26 +0000

    Fix string formatting

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -193,4 +193,4 @@ namespace ds_exp
             }
             void CreateBiTree(std::istream &definition)
             {
-                auto generated_tree = t
+                auto generated_tree = tree_parse<left_first_t, e
