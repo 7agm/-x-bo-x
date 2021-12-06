6270ab403b1744ad9b8a44783d7ad1f02d0210a4
7agm <109569781+7agm@users.noreply.github.com>
2021-12-06 18:50:45 +0000

    3.2.2

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -193,4 +193,4 @@ namespace ds_exp
             }
             void CreateBiTree(std::istream &definition)
             {
-                auto generated_tree = tree_parse<left_first_t, element_type>(defini
+                auto generated_tree = tree_parse<left_first_t, element_type>(definition).get_b
