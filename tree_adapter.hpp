eaa0919ec517ae958a5e9f2f56e61446fcb21f75
7agm <109569781+7agm@users.noreply.github.com>
2021-12-09 19:45:40 +0000

    Reviewed

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -195,4 +195,4 @@ namespace ds_exp
             {
                 auto generated_tree = tree_parse<left_first_t, element_type>(definition).get_binary_tree();
                 if (!generated_tree)
-              
+                    throw parse_fail
