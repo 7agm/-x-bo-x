d1d82ea594da1d2685ada5a2f9869820ab55e580
7agm <109569781+7agm@users.noreply.github.com>
2022-03-09 07:53:27 +0000

    Fix build breakage.

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -228,4 +228,5 @@ namespace ds_exp
                 return tree->root();
             }
             template <typename order_t = preorder_t, typename dir_t = left_first_t>
-            auto &Value(key_type const &key, order_t order = order_t{}, dir_t dir = d
+            auto &Value(key_type const &key, order_t order = order_t{}, dir_t dir = dir_t{}) const
+            {
