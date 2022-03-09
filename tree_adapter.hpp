e0c658236d28d6c47007efb66a79465bd523f561
7agm <109569781+7agm@users.noreply.github.com>
2022-03-09 20:09:15 +0000

    fixed variable name

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -230,3 +230,4 @@ namespace ds_exp
             template <typename order_t = preorder_t, typename dir_t = left_first_t>
             auto &Value(key_type const &key, order_t order = order_t{}, dir_t dir = dir_t{}) const
             {
+                if (
