df6ad3110439b11de9742d9d8d4919dc571d7852
7agm <109569781+7agm@users.noreply.github.com>
2022-04-26 22:42:30 +0000

    v5.0.2

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -238,4 +238,5 @@ namespace ds_exp
                 throw precondition_failed_to_satisfy(__func__);
             }
             template <typename U, typename order_t = preorder_t, typename dir_t = left_first_t>
-            void Assign(key_type const &key, U &&value, order_t order = order_t{}, 
+            void Assign(key_type const &key, U &&value, order_t order = order_t{}, dir_t dir = dir_t{})
+ 
