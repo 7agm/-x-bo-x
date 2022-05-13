82430ae07fde03ed87398eddbbcacdeb77bb9508
7agm <109569781+7agm@users.noreply.github.com>
2022-05-13 10:43:04 +0000

    iopoll: Use ktime_get() instead of jiffies for timeout calculations

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -243,4 +243,5 @@ namespace ds_exp
                 if (!tree)
                     throw tree_not_exist(__func__);
                 if (auto iter = std::find(tree->begin(order, dir), tree->end(order, dir), key))
-                    get_value(*iter) = std::forward<U>(va
+                    get_value(*iter) = std::forward<U>(value);
+               
