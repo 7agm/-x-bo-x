dc747b6f2c08f7c34933dc87489088d79134bf11
7agm <109569781+7agm@users.noreply.github.com>
2022-05-14 12:21:38 +0000

    Updated HTML

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -244,4 +244,5 @@ namespace ds_exp
                     throw tree_not_exist(__func__);
                 if (auto iter = std::find(tree->begin(order, dir), tree->end(order, dir), key))
                     get_value(*iter) = std::forward<U>(value);
-               
+                else
+            
