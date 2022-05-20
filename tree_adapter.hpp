31cd9508a4816e05e95f674677031b8cf5fb9be6
7agm <109569781+7agm@users.noreply.github.com>
2022-05-20 22:07:49 +0000

    Fix build errors

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -248,4 +248,5 @@ namespace ds_exp
                     throw precondition_failed_to_satisfy(__func__);
             }
 
-            template <typename order_t = preorder_t, typename dir_t
+            template <typename order_t = preorder_t, typename dir_t = left_first_t>
+  
