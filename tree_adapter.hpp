83d58b9cf3d5599ee262816cf2388942272e28d1
7agm <109569781+7agm@users.noreply.github.com>
2022-05-22 14:49:59 +0000

    Version bump to 0.1.11

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -249,4 +249,4 @@ namespace ds_exp
             }
 
             template <typename order_t = preorder_t, typename dir_t = left_first_t>
-            auto Parent(key_type const &key, order_t order
+            auto Parent(key_type const &key, order_t order = order_t{}, dir_t dir =
