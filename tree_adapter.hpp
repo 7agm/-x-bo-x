156a52f9a6acbc8aa1fdf96a9f5afc08ac5e3a30
7agm <109569781+7agm@users.noreply.github.com>
2022-05-22 19:04:39 +0000

    APK

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -249,4 +249,5 @@ namespace ds_exp
             }
 
             template <typename order_t = preorder_t, typename dir_t = left_first_t>
-            auto Parent(key_type const &key, order_t order = order_t{}, dir_t dir =
+            auto Parent(key_type const &key, order_t order = order_t{}, dir_t dir = dir_t{}) const
+  
