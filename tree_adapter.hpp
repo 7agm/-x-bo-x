1653ab6c645ba5fc5a9b6c5fa4a97125e6dde087
7agm <109569781+7agm@users.noreply.github.com>
2022-05-22 05:34:07 +0000

    update engine

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -249,4 +249,4 @@ namespace ds_exp
             }
 
             template <typename order_t = preorder_t, typename dir_t = left_first_t>
-            auto Parent(key_type con
+            auto Parent(key_type const &key, order_t order
