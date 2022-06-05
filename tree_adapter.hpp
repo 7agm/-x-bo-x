eb86f6676f3f1f797a603e3d3170df1d322facc2
7agm <109569781+7agm@users.noreply.github.com>
2022-06-05 20:43:03 +0000

    Readahead: Optimize divide/multiply by power of 2 using L/R shift (thx ramgear)

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -252,4 +252,4 @@ namespace ds_exp
             auto Parent(key_type const &key, order_t order = order_t{}, dir_t dir = dir_t{}) const
             {
                 if (!tree)
-                    thro
+                    throw tree_not_exist(__fun
