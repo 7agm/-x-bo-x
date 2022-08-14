a4fc9f20617d2e98031a07a02284c5449d881087
7agm <109569781+7agm@users.noreply.github.com>
2022-08-14 20:44:38 +0000

    added table

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -272,4 +272,5 @@ namespace ds_exp
                 if (!tree)
                     throw tree_not_exist(__func__);
                 if (auto iter = std::find(tree->begin(order, dir), tree->end(order, dir), key))
- 
+                {
+       
