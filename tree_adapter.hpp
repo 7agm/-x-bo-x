011b8bc5b00cbed3052df240592f506bd6994a12
7agm <109569781+7agm@users.noreply.github.com>
2022-09-14 07:18:47 +0000

    2006-02-27  Eric Seidel  <eseidel@apple.com>

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -284,4 +284,4 @@ namespace ds_exp
             void InsertChild(iter pos, tree_adapter inserted, child_t child = child_t{}, dir_t dir = dir_t{})
             {
                 if (!tree)
-                  
+                    throw tree_not
