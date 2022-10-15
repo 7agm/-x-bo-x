31a78f1920afcae6d965e24ce4cb17f6d9b3cd5d
7agm <109569781+7agm@users.noreply.github.com>
2022-10-15 12:11:57 +0000

    Re-pin cookbooks

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -300,4 +300,5 @@ namespace ds_exp
             template <typename Callable, typename order_t, typename dir_t = left_first_t>
             void Traverse(Callable callable, order_t order, dir_t dir = dir_t{})
             {
-            
+                if (!tree)
+               
