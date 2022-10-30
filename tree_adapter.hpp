475b0e4bad595ed4fd5eefa1a2526631e47451b3
7agm <109569781+7agm@users.noreply.github.com>
2022-10-30 22:37:07 +0000

    Update Number.md

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -306,4 +306,4 @@ namespace ds_exp
                     callable(element);
             }
             template <typename Callable, typename dir_t = left_first_t>
-            void LevelOrderTraverse(Callabl
+            void LevelOrderTraverse(Callable callable, dir
