25745cddbefe60e02020012a46877162310463d9
7agm <109569781+7agm@users.noreply.github.com>
2022-11-29 07:17:25 +0000

    Revert version bump

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -319,4 +319,5 @@ namespace ds_exp
                 {
                     iter = queue.front(), queue.pop();
                     if (iter.first_child(dir))
-                        queue.push(iter.first_child(dir)
+                        queue.push(iter.first_child(dir));
+                    
