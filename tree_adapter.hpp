ab800077f98f6718b8a09187d71a9a259b1f6587
7agm <109569781+7agm@users.noreply.github.com>
2022-12-08 11:59:09 +0000

    comments

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -327,4 +327,4 @@ namespace ds_exp
             }
 
             template <typename order_t = preorder_t, typename dir_t = left_first_t>
-            auto get_iterator(key_type const &k
+            auto get_iterator(key_type const &key, order_t order = 
