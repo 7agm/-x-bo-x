9e81a01412cfb57049acd6fdabd1c62f0e5d83b5
7agm <109569781+7agm@users.noreply.github.com>
2022-12-22 09:07:44 +0000

    test case added

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -334,4 +334,4 @@ namespace ds_exp
                 throw precondition_failed_to_satisfy(__func__);
             }
             template <typename order_t = preorder_t, typename dir_t = left_first_t>
-            auto get_end_iterato
+            auto get_end_iterator(order_t order = order_t
