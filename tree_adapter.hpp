1cd22ab37adef51adeaf124fe0f60d42118523fe
7agm <109569781+7agm@users.noreply.github.com>
2022-12-23 20:37:49 +0000

    Serialization

--- tree_adapter.hpp
+++ tree_adapter.hpp
@@ -336,4 +336,5 @@ namespace ds_exp
             template <typename order_t = preorder_t, typename dir_t = left_first_t>
             auto get_end_iterator(order_t order = order_t{}, dir_t dir = dir_t{})
             {
-                i
+                if(!tree)
+                  
