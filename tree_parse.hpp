2e2b4b1f118f30ef63bd97094837df358bb9e331
7agm <109569781+7agm@users.noreply.github.com>
2023-02-25 21:54:30 +0000

    Fix make clean

--- tree_parse.hpp
+++ tree_parse.hpp
@@ -1,3 +1,24 @@
 
 #ifndef INC_201703_TREE_PARSE_HPP
 #define INC_201703_TREE_PARSE_HPP
+
+#include <optional>
+#include <string_view>
+#include <sstream>
+#include <stdexcept>
+#include <functional>
+#include "binary_tree.hpp"
+
+namespace ds_exp
+{
+    inline namespace parse
+    {
+        using namespace std::literals;
+        struct parse_failed : std::domain_error
+        {
+            parse_failed()
+                : domain_error("parse failed in tree_parse.")
+            {
+            }
+        };
+        struct expect_failed : std::domain_error
